86332c153094f64bd52f8863f8e8fabcd8cda34d
Elogeek <68891109+Elogeek@users.noreply.github.com>
2020-11-30 14:58:15 +0100

    exo les boucles

---
+++ script.js
@@ -0,0 +1,12 @@
+
+/* boucle while */
+let iTest = 0;
+while(iTest < 11) {
+    console.log("iTest vaut: " +iTest );
+    iTest++;
+}
+
+/* boucle for*/
+for(let iTest = 0; iTest < 11; iTest++) {
+    console.log ("iTest vaut:" + iTest);
+}
